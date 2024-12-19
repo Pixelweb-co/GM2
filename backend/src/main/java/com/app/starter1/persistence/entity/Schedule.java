7d857b66d8ede1dd065de103473f7f8ae0e425de
Edwin <egbmaster2007@gmail.com>
2024-12-18 19:14:25 -0500

    Calendar

--- backend/src/main/java/com/app/starter1/persistence/entity/Schedule.java
+++ backend/src/main/java/com/app/starter1/persistence/entity/Schedule.java
@@ -1,4 +1,29 @@
 package com.app.starter1.persistence.entity;
 
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Schedule")
 public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = true)
+    private Integer device;
+
+    @Column(length = 10, nullable = true)
+    private String date;
+
+    @Column(nullable = true)
+    private Integer status;
+
 }
