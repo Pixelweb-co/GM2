841c7afbf49a78bd18fcd3a92abaa50cf9922493
Edwin <egbmaster2007@gmail.com>
2025-04-20 00:28:33 -0500

    token validation

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -20,12 +20,14 @@ import ScrollToTop from '@core/components/scroll-to-top'
 
 // Util Imports
 import { getMode, getSystemMode } from '@core/utils/serverHelpers'
+import { useEffect } from 'react'
 
 const Layout = async ({ children }: ChildrenType) => {
   // Vars
   const direction = 'ltr'
   const mode = getMode()
   const systemMode = getSystemMode()
+
 
   return (
     <Providers direction={direction}>
