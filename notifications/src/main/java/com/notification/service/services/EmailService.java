3d87187c80d6a653ed86313725acb83dab187b82
Edwin <egbmaster2007@gmail.com>
2025-07-09 20:31:37 -0500

    update

--- notifications/src/main/java/com/notification/service/services/EmailService.java
+++ notifications/src/main/java/com/notification/service/services/EmailService.java
@@ -32,7 +32,7 @@ public class EmailService {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true); // true indica que el contenido es HTML
 
-            helper.setFrom("gm2@cloudfly.com.co");
+            helper.setFrom("gm2@yunismedical.com");
             helper.setTo(notification.getTo());
             helper.setSubject(notification.getSubject());
             helper.setText(body, true); // El segundo parámetro true indica que el cuerpo es HTML
