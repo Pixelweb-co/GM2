d6001acfbb91a88d51bd7d42c94809dda4e15c1f
Edwin guevara <egbmaster2007@gmail.com>
2025-10-05 17:49:13 -0500

    update 2

--- frontend/src/views/apps/calendar/Calendar.tsx
+++ frontend/src/views/apps/calendar/Calendar.tsx
@@ -169,6 +169,7 @@ const Calendar = (props: CalenderProps) => {
 
       jsEvent.preventDefault()
 
+      
       const text = "Deseas confirmar el mantenimiento?";
        if (confirm(text) != true) {
         return
